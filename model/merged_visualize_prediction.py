0d0cb57fab576fa6f2ae994d185e3e659f174167
Yuezhi Cai <yuezhi.cai@tum.de>
2021-01-19 13:35:25 +0100

    Add the altered version of the function plot_saliency_map so color bar of all methods are not shown.

--- model/merged_visualize_prediction.py
+++ model/merged_visualize_prediction.py
@@ -16,6 +16,7 @@ from pylab import *
 import seaborn as sns
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+from skimage.transform import resize
 
 # data science
 import numpy as np
@@ -576,3 +577,65 @@ def plot_map(model, dataloader, label, covid=False, saliency_layer=None, axes_a=
         return inputs, labels, filename, preds
 
 
+def plot_saliency_map(saliency_map, img=None, ax=None,
+                      colorbar_label='Bits / Pixel',
+                      colorbar_fontsize=14,
+                      min_alpha=0.2, max_alpha=0.7, vmax=None,
+                      colorbar_size=0.3, colorbar_pad=0.08):
+
+    """
+    Plots the heatmap with an bits/pixel colorbar and optionally overlays the image.
+
+    Args:
+        saliency_map (np.ndarray): the saliency_map.
+        img (np.ndarray):  show this image under the saliency_map.
+        ax: matplotlib axis. If ``None``, a new plot is created.
+        colorbar_label (str): label for the colorbar.
+        colorbar_fontsize (int): fontsize of the colorbar label.
+        min_alpha (float): minimum alpha value for the overlay. only used if ``img`` is given.
+        max_alpha (float): maximum alpha value for the overlay. only used if ``img`` is given.
+        vmax: maximum value for colorbar.
+        colorbar_size: width of the colorbar. default: Fixed(0.3).
+        colorbar_pad: width of the colorbar. default: Fixed(0.08).
+
+    Returns:
+        The matplotlib axis ``ax``.
+    """
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+    from mpl_toolkits.axes_grid1.axes_size import Fixed
+    from skimage.color import rgb2grey, grey2rgb
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=(5.5, 4.0))
+
+    if img is not None:
+        # Underlay the image as greyscale
+        grey = grey2rgb(rgb2grey(img))
+        ax.imshow(grey)
+
+    ax1_divider = make_axes_locatable(ax)
+    if type(colorbar_size) == float:
+        colorbar_size = Fixed(colorbar_size)
+    if type(colorbar_pad) == float:
+        colorbar_pad = Fixed(colorbar_pad)
+    cax1 = ax1_divider.append_axes("right", size=colorbar_size, pad=colorbar_pad)
+    if vmax is None:
+        vmax = saliency_map.max()
+    norm = mpl.colors.Normalize(vmin=0, vmax=vmax)
+    n = 256
+    half_jet_rgba = plt.cm.seismic(np.linspace(0.5, 1, n))
+    half_jet_rgba[:, -1] = np.linspace(0.2, 1, n)
+    cmap = mpl.colors.ListedColormap(half_jet_rgba)
+    hmap_jet = cmap(norm(saliency_map))
+    if img is not None:
+        hmap_jet[:, :, -1] = (max_alpha - min_alpha)*norm(saliency_map) + min_alpha
+    ax.imshow(hmap_jet, alpha=max_alpha)
+    # cbar = mpl.colorbar.ColorbarBase(cax1, cmap=cmap, norm=norm)
+    # cbar.set_label(colorbar_label, fontsize=colorbar_fontsize)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid('off')
+    ax.set_frame_on(False)
+    return ax
