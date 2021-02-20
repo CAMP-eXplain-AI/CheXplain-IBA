29e0fd1548b75202c5fed01bc2a1512c69caf65d
Yang Zhang <ya.zhang@tum.de>
2021-02-20 21:15:25 +0000

    fix shell script

--- evaluation/run_sensitivity_n.sh
+++ evaluation/run_sensitivity_n.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PATH_TO_IMAGES="~/NIH_small"
-PATH_TO_MODEL='~/informationbottleneck/model/results/checkpoint_best'
-label_path='~/informationbottleneck/model/labels'
-heatmap_dir='~/results/ib'
+PATH_TO_IMAGES="/home/ubuntu/NIH_small"
+PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/checkpoint_best'
+label_path='/home/ubuntu/informationbottleneck/model/labels'
+heatmap_dir='/home/ubuntu/results/ib'
 out_dir='~'
 
 python eval_sensitivity_n.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path
