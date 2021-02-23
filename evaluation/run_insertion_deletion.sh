62b14600c61cad192692304bd8292daba720f3a2
Yang Zhang <ya.zhang@tum.de>
2021-02-23 23:42:19 +0000

    fix script

--- evaluation/run_insertion_deletion.sh
+++ evaluation/run_insertion_deletion.sh
@@ -27,19 +27,19 @@ PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/regression_check
 label_path='/home/ubuntu/informationbottleneck/model/labels'
 heatmap_dir='/home/ubuntu/results/mse_loss_with_target'
 out_dir='/home/ubuntu'
-file_name='sensitivity_n_mse_target.json'
+file_name='insertion_deletion_mse_target.json'
 
 python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid --regression
 
 heatmap_dir='/home/ubuntu/results/mse_loss_maximize_score'
 out_dir='/home/ubuntu'
-file_name='sensitivity_n_maximize_score.json'
+file_name='insertion_deletion_maximize_score.json'
 
 python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid --regression
 
 
 heatmap_dir='/home/ubuntu/results/mse_loss_minimal_deviation'
 out_dir='/home/ubuntu'
-file_name='sensitivity_n_minimal_deviation'
+file_name='insertion_deletion_minimal_deviation.json'
 
 python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid --regression
