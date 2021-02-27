7555103dcb92c13e045f4ef6e5597dbac0d07429
Yang Zhang <ya.zhang@tum.de>
2021-02-27 23:08:36 +0000

    use weighted model for eval sensitivity-N

--- evaluation/run_sensitivity_n.sh
+++ evaluation/run_sensitivity_n.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PATH_TO_IMAGES="/home/ubuntu/NIH_small"
-PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/checkpoint_best'
+PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/checkpoint_best_weighted'
 label_path='/home/ubuntu/informationbottleneck/model/labels'
 heatmap_dir='/home/ubuntu/results/grad_cam'
 out_dir='/home/ubuntu'
@@ -9,11 +9,11 @@ file_name='sensitivity_n_blur.json'
 
 python eval_sensitivity_n.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --blur
 
-#heatmap_dir='/home/ubuntu/results/ib_new_init'
-#out_dir='/home/ubuntu'
-#file_name='sensitivity_n_ib_new_init.json'
-#
-#python eval_sensitivity_n.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --blur
+heatmap_dir='/home/ubuntu/results/ib_new_init'
+out_dir='/home/ubuntu'
+file_name='sensitivity_n_ib_new_init.json'
+
+python eval_sensitivity_n.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --blur
 
 heatmap_dir='/home/ubuntu/results/integrated_gradients'
 out_dir='/home/ubuntu'
