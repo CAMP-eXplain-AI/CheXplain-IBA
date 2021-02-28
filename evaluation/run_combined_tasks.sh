8fc68da21ba47094a52fb534be031a5af0cb46e2
Yang Zhang <ya.zhang@tum.de>
2021-02-28 22:11:37 +0000

    finish bugfix

--- evaluation/run_combined_tasks.sh
+++ evaluation/run_combined_tasks.sh
@@ -3,37 +3,37 @@ PATH_TO_IMAGES="/home/ubuntu/BrixIAsmall"
 PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/classification_checkpoint_best_weighted'
 label_path='/home/ubuntu/informationbottleneck/model/labels'
 
-#heatmap_dir='/home/ubuntu/results/covid_classification_weighted/grad_cam'
-#out_dir='/home/ubuntu/covid_classification_weighted'
-#file_name='insertion_deletion.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
-#
-#heatmap_dir='/home/ubuntu/results/covid_classification_weighted/excitation_backprop'
-#file_name='insertion_deletion_excitation_backprop.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
-#
-#heatmap_dir='/home/ubuntu/results/covid_classification_weighted/gradient'
-#file_name='insertion_deletion_gradient.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
-#
-#heatmap_dir='/home/ubuntu/results/covid_classification_weighted/integrated_gradients'
-#out_dir='/home/ubuntu/covid_classification_weighted'
-#file_name='insertion_deletion_integrated_gradients.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
-#
-#heatmap_dir='/home/ubuntu/results/covid_classification_weighted/ib'
-#file_name='insertion_deletion_ib.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
-#
-#heatmap_dir='/home/ubuntu/results/covid_classification_weighted/reverse_ib'
-#file_name='insertion_deletion_reverse_ib.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+heatmap_dir='/home/ubuntu/results/covid_classification_weighted/grad_cam'
+out_dir='/home/ubuntu/covid_classification_weighted'
+file_name='insertion_deletion.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+
+heatmap_dir='/home/ubuntu/results/covid_classification_weighted/excitation_backprop'
+file_name='insertion_deletion_excitation_backprop.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+
+heatmap_dir='/home/ubuntu/results/covid_classification_weighted/gradient'
+file_name='insertion_deletion_gradient.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+
+heatmap_dir='/home/ubuntu/results/covid_classification_weighted/integrated_gradients'
+out_dir='/home/ubuntu/covid_classification_weighted'
+file_name='insertion_deletion_integrated_gradients.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+
+heatmap_dir='/home/ubuntu/results/covid_classification_weighted/ib'
+file_name='insertion_deletion_ib.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+
+heatmap_dir='/home/ubuntu/results/covid_classification_weighted/reverse_ib'
+file_name='insertion_deletion_reverse_ib.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
 
 
 ################################################## Sensitivity N #################################################################
