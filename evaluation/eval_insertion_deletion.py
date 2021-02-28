66e1ad7951672be7fa2e3c973750a77a2509df17
Yang Zhang <ya.zhang@tum.de>
2021-02-28 12:37:51 +0000

    set flag to load grayscale

--- evaluation/eval_insertion_deletion.py
+++ evaluation/eval_insertion_deletion.py
@@ -116,10 +116,10 @@ def evaluation(heatmap_dir, out_dir, image_path, model_path, label_path, file_na
         for data in tqdm(dataloader, desc="Samples"):
             if covid:
                 input, label, filename = data
-                heatmap = cv2.imread(os.path.join(heatmap_dir, filename[0]), cv2.IMREAD_UNCHANGED)
+                heatmap = cv2.imread(os.path.join(heatmap_dir, filename[0]), cv2.IMREAD_GRAYSCALE)
             else:
                 input, label, filename, bbox = data
-                heatmap = cv2.imread(os.path.join(heatmap_dir, category, filename[0]), cv2.IMREAD_UNCHANGED)
+                heatmap = cv2.imread(os.path.join(heatmap_dir, category, filename[0]), cv2.IMREAD_GRAYSCALE)
 
             # skip img if the pred score is too low
             if not regression and torch.nn.functional.sigmoid(model(input.to(device))[0, target]) < 0.6:
