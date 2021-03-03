c37a7677dae83cb502b2c02a96379269c2588e84
Yang Zhang <ya.zhang@tum.de>
2021-03-03 22:01:46 +0000

    modify script

--- evaluation/run_insertion_deletion.sh
+++ evaluation/run_insertion_deletion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-PATH_TO_IMAGES="/home/ubuntu/NIH_small"
-PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/checkpoint_best'
-label_path='/home/ubuntu/informationbottleneck/model/labels'
+#
+#PATH_TO_IMAGES="/home/ubuntu/NIH_small"
+#PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/checkpoint_best'
+#label_path='/home/ubuntu/informationbottleneck/model/labels'
 #heatmap_dir='/home/ubuntu/results/nih_weighted/grad_cam'
 #out_dir='/home/ubuntu'
 #file_name='insertion_deletion.json'
@@ -52,49 +52,42 @@ label_path='/home/ubuntu/informationbottleneck/model/labels'
 #
 #python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
 ######################################### regression model ###############################################
-#PATH_TO_IMAGES="/home/ubuntu/BrixIAsmall"
-#PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/regression_checkpoint_best'
-#label_path='/home/ubuntu/informationbottleneck/model/labels'
-#heatmap_dir='/home/ubuntu/results/mse_loss_with_target'
-#out_dir='/home/ubuntu'
-#file_name='insertion_deletion_mse_target.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid --regression
-#
-#heatmap_dir='/home/ubuntu/results/mse_loss_maximize_score'
-#out_dir='/home/ubuntu'
-#file_name='insertion_deletion_maximize_score.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid --regression
-#
-#
-#heatmap_dir='/home/ubuntu/results/mse_loss_minimal_deviation'
-#out_dir='/home/ubuntu'
-#file_name='insertion_deletion_minimal_deviation.json'
-#
-#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid --regression
+PATH_TO_IMAGES="/home/ubuntu/BrixIAsmall"
+PATH_TO_MODEL='/home/ubuntu/informationbottleneck/model/results/classification_checkpoint_best_weighted'
+label_path='/home/ubuntu/informationbottleneck/model/labels'
+heatmap_dir='/home/ubuntu/results/filtered_mask_covid_0.0040'
+out_dir='/home/ubuntu'
+file_name='insertion_deletion_filtered_mask_0040.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
+
+heatmap_dir='/home/ubuntu/results/filtered_mask_covid_0.0050'
+out_dir='/home/ubuntu'
+file_name='insertion_deletion_filtered_mask_0050.json'
+
+python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name --covid
 
 ######################################### regression model ###############################################
-heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0020'
-out_dir='/home/ubuntu'
-file_name='insertion_deletion_filtered_mask02_weighted.json'
-
-python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
-
-heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0030'
-out_dir='/home/ubuntu'
-file_name='insertion_deletion_filtered_mask03_weighted.json'
-
-python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
-
-heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0040'
-out_dir='/home/ubuntu'
-file_name='insertion_deletion_filtered_mask04_weighted.json'
-
-python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
-
-heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0060'
-out_dir='/home/ubuntu'
-file_name='insertion_deletion_filtered_mask06_weighted.json'
-
-python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
+#heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0020'
+#out_dir='/home/ubuntu'
+#file_name='insertion_deletion_filtered_mask02_weighted.json'
+#
+#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
+#
+#heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0030'
+#out_dir='/home/ubuntu'
+#file_name='insertion_deletion_filtered_mask03_weighted.json'
+#
+#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
+#
+#heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0040'
+#out_dir='/home/ubuntu'
+#file_name='insertion_deletion_filtered_mask04_weighted.json'
+#
+#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
+#
+#heatmap_dir='/home/ubuntu/results/filtered_mask_new_weighted_0.0060'
+#out_dir='/home/ubuntu'
+#file_name='insertion_deletion_filtered_mask06_weighted.json'
+#
+#python eval_insertion_deletion.py $heatmap_dir $out_dir $PATH_TO_IMAGES $PATH_TO_MODEL $label_path $file_name
